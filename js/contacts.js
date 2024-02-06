d68a5186b114d6f778ec481662252606759d647b
Giuliano Gioia <giulianogioia86@gmail.com>
2024-02-06 19:43:43 +0100

    change renderContacts
    alpha + seperator

--- js/contacts.js
+++ js/contacts.js
@@ -1,8 +1,8 @@
 let contacts = [];
 
-function init(){
-    includeHTML();
-    renderContacts();
+function init() {
+  includeHTML();
+  renderContacts();
 }
 
 function openNewContact() {
@@ -36,17 +36,58 @@ function addNewContact() {
   renderContacts();
 }
 
+// function renderContacts() {
+
+//   for (let i = 0; i < contacts.length; i++) {
+//     let contact = contacts[i];
+
+//     document.getElementById("conctactMemberField").innerHTML += /*html*/ `
+//             <div class="contact">
+//             ${contact.name}
+//             ${contact.email}
+//             ${contact.phone}
+//             </div>
+//         `;
+//   }
+// }
+
 function renderContacts() {
-  
+  document.getElementById("conctactMemberField").innerHTML = "";
+  contacts.sort((a, b) => a.name.localeCompare(b.name));
+
+  let currentLetter = "";
+  let contactHTML = "";
+
   for (let i = 0; i < contacts.length; i++) {
     let contact = contacts[i];
+    let firstLetter = contact.name.charAt(0).toUpperCase();
 
-    document.getElementById("conctactMemberField").innerHTML += /*html*/ `
+    // Überprüfe, ob ein neuer Buchstabe erreicht wurde
+    if (firstLetter !== currentLetter) {
+      // Wenn ja, füge einen neuen Container für den Buchstaben hinzu
+      if (contactHTML !== "") {
+        document.getElementById("conctactMemberField").innerHTML += contactHTML;
+      }
+      contactHTML = `<div class="contact-group">
+                        <h2>${firstLetter}</h2>
+                          <img src="../assets/img/seperater_contacts.svg" alt="">
+                        <div class="contact">`;
+      currentLetter = firstLetter;
+    }
+
+    // Füge den Kontakt zum aktuellen Container hinzu
+    contactHTML += `
             <div class="contact">
-            ${contact.name}
-            ${contact.email}
-            ${contact.phone}
+               <h3> ${contact.name}</h3>
+                <a href="#">${contact.email}</a>
+   
             </div>
         `;
+  }
+
+  // Füge den letzten Container hinzu
+  if (contactHTML !== "") {
+    document.getElementById("conctactMemberField").innerHTML +=
+      contactHTML + `</div></div>`;
   }
 }
