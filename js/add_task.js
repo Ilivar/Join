e41400dc70b08b01b4c5e90f78e0aff5e1c6683d
MaxanGe95 <Maximilian.Glocke@web.de>
2024-02-07 21:28:36 +0100

    Add Task Buttons + Acordion

--- js/add_task.js
+++ js/add_task.js
@@ -11,21 +11,60 @@ acc.addEventListener("click", function() {
 });
 });
 
+function toggleAccordion(element) {
+    var content = element.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  }
+
+let activeButton = null;
+
+function setActiveButton(buttonId, imgSrc, bgColor) {
+    if (activeButton) {
+        document.getElementById(activeButton).style.background = "";
+        document.getElementById(activeButton).style.color = "";
+        document.getElementById(activeButton).classList.remove("active");
+        if (activeButton === 'buttonUrgent') {
+            document.getElementById('buttonUrgent').style.background = "";
+            document.getElementById('buttonUrgent').style.color = "";
+            document.getElementById('prioImg').src = "../assets/img/Prio up.svg"; 
+        } else if (activeButton === 'buttonMedium') {
+            document.getElementById('buttonMedium').style.background = "";
+            document.getElementById('buttonMedium').style.color = "";
+            document.getElementById('mediumImg').src = "../assets/img/Prio media.svg"; 
+        } else if (activeButton === 'buttonLow') {
+            document.getElementById('buttonLow').style.background = "";
+            document.getElementById('buttonLow').style.color = "";
+            document.getElementById('lowImg').src = "../assets/img/Prio down.svg"; 
+        }
+    }
+    document.getElementById(buttonId).style.background = bgColor;
+    document.getElementById(buttonId).style.color = "white";
+    document.getElementById(buttonId).classList.add("active");
+    if (imgSrc && buttonId === 'buttonUrgent') {
+        document.getElementById('prioImg').src = imgSrc; 
+    } else if (imgSrc && buttonId === 'buttonMedium') {
+        document.getElementById('mediumImg').src = imgSrc; 
+    } else if (imgSrc && buttonId === 'buttonLow') {
+        document.getElementById('lowImg').src = imgSrc; 
+    }
+    activeButton = buttonId;
+}
+
 function buttonUrgent() {
-    document.getElementById('buttonUrgent').style.background = "rgba(255, 61, 0, 1)";
-    document.getElementById('buttonUrgent').style.color = "white";
-    document.getElementById('prioImg').src = "../assets/img/prio alta.svg";
+    setActiveButton('buttonUrgent', "../assets/img/prio alta.svg", "rgba(255, 61, 0, 1)");
 }
 
 function buttonMedium() {
-    document.getElementById('buttonMedium').style.background = "rgba(255, 168, 0, 1)";
-    document.getElementById('buttonMedium').style.color = "white";
-    document.getElementById('mediumImg').src = "../assets/img/prio media white.svg";
+    setActiveButton('buttonMedium', "../assets/img/prio media white.svg", "rgba(255, 168, 0, 1)");
 }
 
 function buttonLow() {
-    document.getElementById('buttonLow').style.background = "rgba(122, 226, 41, 1)";
-    document.getElementById('buttonLow').style.color = "white";
-    document.getElementById('lowImg').src = "../assets/img/Prio baja.svg";
+    setActiveButton('buttonLow', "../assets/img/Prio baja.svg", "rgba(122, 226, 41, 1)");
 }
+
+
 
