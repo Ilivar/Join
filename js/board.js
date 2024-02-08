23971a76f947a3ba3e9e566ec61b91dd16396a83
AdrianZeiher <156835007+AdrianZeiher@users.noreply.github.com>
2024-02-08 08:00:40 +0100

    update board card css

--- js/board.js
+++ js/board.js
@@ -1,7 +1,7 @@
 let todos = [{
     'id': 0,
     'category': 'User Story',
-    'title': 'Kochwelt Page & Recipe recommender',
+    'title': 'Kochwelt Page & Recipe Recommender',
     'description': 'Build start page with recipe recommendation.',
     'due date': 'Due date: 10/05/2023',
     'prio': 'Priority: Medium =',
@@ -10,7 +10,7 @@ let todos = [{
     'name': 'drag_to_do'
 }, {
     'id': 1,
-    'category': 'Technical task',
+    'category': 'Technical Task',
     'title': 'CSS Architecture Planning',
     'description': 'Define CSS naming conventions and structure.',
     'due date': 'Due date: 02/09/2023',
@@ -20,7 +20,7 @@ let todos = [{
     'name': 'drag_in_progress'
 }, {
     'id': 2,
-    'category': 'Technical task',
+    'category': 'Technical Task',
     'title': 'CSS Architecture Planning',
     'description': 'Define CSS naming conventions and structure.',
     'due date': 'Due date: 02/09/2023',
@@ -30,7 +30,7 @@ let todos = [{
     'name': 'drag_await_feedback'
 }, {
     'id': 3,
-    'category': 'Technical task',
+    'category': 'Technical Task',
     'title': 'CSS Architecture Planning',
     'description': 'Define CSS naming conventions and structure.',
     'due date': 'Due date: 02/09/2023',
@@ -91,24 +91,46 @@ function startDragging(id) {
 }
 
 function generateTodoHTML(element) {
-    return `<div draggable="true" ondragstart="startDragging(${element['id']})"><div class="task_content">
-        
-    <div class="card_content"> 
-        <div class="category">${element['category']}</div>
-        <div class="title">${element['title']}</div>
-        <div class="description">${element['description']}</div>
+    return /*html*/`
+  <div draggable="true" ondragstart="startDragging(${element['id']})">
+  <div class="task_content">
+
+    <div class="card_content">
+
+      <div class="category">${element['category']}</div>
+
+         <div class="title_description">
+           <div class="title">${element['title']}</div>
+           <div class="description">${element['description']}</div>
+         </div>
+      <div class="additional-text">
+
         <div>${element['due date']}</div>
         <div>${element['prio']}</div>
         <div>${element['assigned to']}</div>
         <div>${element['subtasks']}</div>
-       
-    </div>
 
-    <div class="progress-bar-container">
-        <div class="progress-bar"></div>
-    </div>
+      </div>
 
-    </div></div>`;
+        <div class="over_progressbar">
+            <div class="progress-bar-container ">
+              <div class="progress-bar"></div>
+             </div>
+                <span class="progressbar_text">1/2 Subtasks</span>
+        </div>
+
+                <div class=profile_content>
+                     <div class="over_profile_badge">
+                           <div class="profile_badge2"><span>AM</span></div>
+                           <div class="profile_badge1"><span>EM</span></div>
+                           <div class="profile_badge"><span>MB</span></div>
+                       </div>
+
+                       <div></div>
+
+                </div>
+    </div>
+</div>`;
 }
 
 function allowDrop(ev) {
