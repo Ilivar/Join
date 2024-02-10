db1267cfd7fc87dae617c110210fc46458aa0485
MaxanGe95 <Maximilian.Glocke@web.de>
2024-02-10 19:41:54 +0100

    Add Task Upload

--- js/add_task.js
+++ js/add_task.js
@@ -204,4 +204,35 @@ function replaceToUserStory() {
 }
 
 
+function addNewAddTask() {
+    let title = document.getElementById("input_title").value;
+    let description = document.getElementById("description").value;
+    let assigned = document.getElementById("").value; //// Nachbessern
+    let dueDate = document.getElementById("input_date").value;
+    let prio = document.getElementById("").value; /// Nachbessern
+    let category = document.getElementById("").value; /// Nachbessern
+    let subtasks = document.getElementById("").value; /// Nachbessern
+
+    const newAddTask = {
+      title: title,
+      description: description,
+      assigned: assigned,
+      dueDate: dueDate,
+      prio: prio,
+      category: category,
+      subtasks: subtasks,
+    };
+    addAddTaskToUserData(newAddTask);
+  }
+
+//   function addAddTaskToUserData(newAddTask) {
+//     if (!currentUserData[0].contacts) {
+//       currentUserData[0].contacts = [];
+//     }
+  
+//     currentUserData[0].contacts.push(newContact);
+  
+//     setItem("users", currentUserData);
+//   }
+
 
