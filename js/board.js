605652d946b3fd5c3d27e12f895362b90e6474e4
Giuliano Gioia <giulianogioia86@gmail.com>
2024-02-15 15:48:11 +0100

    add status function to board
    changeStatus()

--- js/board.js
+++ js/board.js
@@ -8,7 +8,6 @@ function updateToDoArray() {
   todos = value[0].newAddTask;
 }
 
-
 async function init() {
   await includeHTML();
   await loadPreviousMember();
@@ -19,43 +18,45 @@ async function init() {
 }
 
 function updateHTML() {
-  let drag_to_do = todos.filter(t => t['status'] == 'drag_to_do');
+  let drag_to_do = todos.filter((t) => t["status"] == "drag_to_do");
 
-  document.getElementById('drag_to_do').innerHTML = '';
+  document.getElementById("drag_to_do").innerHTML = "";
 
   for (let index = 0; index < drag_to_do.length; index++) {
     const element = drag_to_do[index];
-    document.getElementById('drag_to_do').innerHTML += generateTodoHTML(element);
+    document.getElementById("drag_to_do").innerHTML +=
+      generateTodoHTML(element);
   }
 
-  let drag_in_progress = todos.filter(t => t['status'] == 'drag_in_progress');
+  let drag_in_progress = todos.filter((t) => t["status"] == "drag_in_progress");
 
-  document.getElementById('drag_in_progress').innerHTML = '';
+  document.getElementById("drag_in_progress").innerHTML = "";
 
   for (let index = 0; index < drag_in_progress.length; index++) {
     const element = drag_in_progress[index];
-    document.getElementById('drag_in_progress').innerHTML += generateTodoHTML(element);
-
+    document.getElementById("drag_in_progress").innerHTML +=
+      generateTodoHTML(element);
   }
 
-  let drag_await_feedback = todos.filter(t => t['status'] == 'drag_await_feedback');
+  let drag_await_feedback = todos.filter(
+    (t) => t["status"] == "drag_await_feedback"
+  );
 
-  document.getElementById('drag_await_feedback').innerHTML = '';
+  document.getElementById("drag_await_feedback").innerHTML = "";
 
   for (let index = 0; index < drag_await_feedback.length; index++) {
     const element = drag_await_feedback[index];
-    document.getElementById('drag_await_feedback').innerHTML += generateTodoHTML(element);
-
+    document.getElementById("drag_await_feedback").innerHTML +=
+      generateTodoHTML(element);
   }
 
-  let drag_done = todos.filter(t => t['status'] == 'drag_done');
+  let drag_done = todos.filter((t) => t["status"] == "drag_done");
 
-  document.getElementById('drag_done').innerHTML = '';
+  document.getElementById("drag_done").innerHTML = "";
 
   for (let index = 0; index < drag_done.length; index++) {
     const element = drag_done[index];
-    document.getElementById('drag_done').innerHTML += generateTodoHTML(element);
-
+    document.getElementById("drag_done").innerHTML += generateTodoHTML(element);
   }
 }
 
@@ -64,29 +65,24 @@ function startDragging(id) {
 }
 
 function generateTodoHTML(element) {
-
-
-
-
-  
-  return /*html*/`
-  <div draggable="true" ondragstart="startDragging(${element['id']})">
-  <div class="task_content" onclick="openDialog(${element['id']})">
+  return /*html*/ `
+  <div draggable="true" ondragstart="startDragging(${element["id"]})">
+  <div class="task_content" onclick="openDialog(${element["id"]})">
 
     <div class="card_content">
 
-      <div class="category">${element['category']}</div>
-
+      <div class="category">${element["category"]}</div>
+        <p class="invis">${element["id"]}</p>
          <div class="title_description">
-           <div class="title">${element['title']}</div>
-           <div class="description">${element['description']}</div>
+           <div class="title">${element["title"]}</div>
+           <div class="description">${element["description"]}</div>
          </div>
       <div class="additional-text">
 
-        <div>${element['due_date']}</div>
-        <div>${element['prio']}</div>
-        <div>${element['assigned_to']}</div>
-        <div>${element['subtasks']}</div>
+        <div>${element["due_date"]}</div>
+        <div>${element["prio"]}</div>
+        <div>${element["assigned_to"]}</div>
+        <div>${element["subtasks"]}</div>
 
       </div>
 
@@ -114,37 +110,59 @@ function generateTodoHTML(element) {
 }
 
 
+function changeStatus(id, status) {
+  let currentStatusArray = value[0].newAddTask;
+  let foundIndex = currentStatusArray.findIndex((item) => item.id === id);
+  let formFix= value;
+  let updatedArray = [...currentStatusArray];
 
+  if (foundIndex !== -1) {
+    currentStatusArray[foundIndex].status = status;
+  } else {
+    console.error("ID:", id, "didnt exist!");
+    return;
+  }
+
+  value[0].newAddTask = [];
+
+  for (let i = 0; i < updatedArray.length; i++) {
+    value[0].newAddTask.push(updatedArray[i]);
+  }
+
+  formFix[0].newAddTask = [];
+  formFix[0].newAddTask.push(currentStatusArray);
+  setItem("users", formFix);
+}
 
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
 function moveTo(category) {
-  todos[currentDraggedElement]['status'] = category;
+  todos[currentDraggedElement]["status"] = category;
   updateHTML();
 }
 
 function highlight(id) {
-  document.getElementById(id).classList.add('drag-area-highlight');
+  document.getElementById(id).classList.add("drag-area-highlight");
 }
 
 function removeHighlight(id) {
-  document.getElementById(id).classList.remove('drag-area-highlight');
+  document.getElementById(id).classList.remove("drag-area-highlight");
 }
 
 function openDialog(todoIndex) {
   // Abrufen des entsprechenden Todos anhand des Index
   const todo = todos[todoIndex];
-  
+
   // Extrahieren der assigned_to-Daten des Todos in die Variable contactsboard
   const contactsboard = todo.assigned_to;
 
   // Anzeige des Dialogfelds
-  document.getElementById('todo_HTML').style.display = 'flex';
+  document.getElementById("todo_HTML").style.display = "flex";
 
   // Aufbau des HTML-Inhalts für das Dialogfeld
-  document.getElementById('todo_HTML').innerHTML = `
+  document.getElementById("todo_HTML").innerHTML = `
     <div class="dialog_content" id="close_dialog">
       <div class="category_x">
         <div class="category_dialog">${todo.category}</div>
@@ -181,7 +199,7 @@ function openDialog(todoIndex) {
   `;
 
   // Leere das Element, um sicherzustellen, dass keine vorherigen Inhalte vorhanden sind
-  document.getElementById('member_icons_names').innerHTML = '';
+  document.getElementById("member_icons_names").innerHTML = "";
 
   // Iteriere durch das contactsboard-Array und baue das HTML für die Kontakte auf
   for (let i = 0; i < contactsboard.length; i++) {
@@ -194,7 +212,7 @@ function openDialog(todoIndex) {
         </div>
       </div>
     `;
-    document.getElementById('member_icons_names').innerHTML += contactHTML;
+    document.getElementById("member_icons_names").innerHTML += contactHTML;
   }
 
   // Generiere Farben für die Icons, ändere die Icon-Farben und füge Buchstaben hinzu
@@ -204,8 +222,8 @@ function openDialog(todoIndex) {
 }
 
 function closeDialog() {
-  document.getElementById('close_dialog').innerHTML = '';
-  document.getElementById('todo_HTML').style.display = 'none';
+  document.getElementById("close_dialog").innerHTML = "";
+  document.getElementById("todo_HTML").style.display = "none";
 }
 
 function generateIconColors(contactsboard) {
@@ -241,28 +259,37 @@ function addNameLetters(contactsboard) {
 }
 
 function filterTodosTitle() {
-  let searchText = document.getElementById('filter_input').value.trim().toLowerCase();
+  let searchText = document
+    .getElementById("filter_input")
+    .value.trim()
+    .toLowerCase();
 
   // Filtern der Todos, deren Titel die ersten drei Buchstaben mit dem Suchtext übereinstimmen
-  let filteredTodos = todos.filter(t => t['title'].toLowerCase().startsWith(searchText));
+  let filteredTodos = todos.filter((t) =>
+    t["title"].toLowerCase().startsWith(searchText)
+  );
 
   // Leeren Sie die Inhalte aller Spalten
-  document.getElementById('drag_to_do').innerHTML = '';
-  document.getElementById('drag_in_progress').innerHTML = '';
-  document.getElementById('drag_await_feedback').innerHTML = '';
-  document.getElementById('drag_done').innerHTML = '';
+  document.getElementById("drag_to_do").innerHTML = "";
+  document.getElementById("drag_in_progress").innerHTML = "";
+  document.getElementById("drag_await_feedback").innerHTML = "";
+  document.getElementById("drag_done").innerHTML = "";
 
   // Durchlaufen Sie die gefilterten Todos und fügen Sie sie nur in die entsprechende Spalte ein
   for (let index = 0; index < filteredTodos.length; index++) {
     let element = filteredTodos[index];
-    if (element.status === 'drag_to_do') {
-      document.getElementById('drag_to_do').innerHTML += generateTodoHTML(element);
-    } else if (element.status === 'drag_in_progress') {
-        document.getElementById('drag_in_progress').innerHTML += generateTodoHTML(element);
-      } else if (element.status === 'drag_await_feedback') {
-        document.getElementById('drag_await_feedback').innerHTML += generateTodoHTML(element);
-      } else if (element.status === 'drag_done') {
-        document.getElementById('drag_done').innerHTML += generateTodoHTML(element);
-      }
+    if (element.status === "drag_to_do") {
+      document.getElementById("drag_to_do").innerHTML +=
+        generateTodoHTML(element);
+    } else if (element.status === "drag_in_progress") {
+      document.getElementById("drag_in_progress").innerHTML +=
+        generateTodoHTML(element);
+    } else if (element.status === "drag_await_feedback") {
+      document.getElementById("drag_await_feedback").innerHTML +=
+        generateTodoHTML(element);
+    } else if (element.status === "drag_done") {
+      document.getElementById("drag_done").innerHTML +=
+        generateTodoHTML(element);
     }
   }
+}
